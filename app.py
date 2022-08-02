dcfc2e459d6c5987fdb57a801badf78654251ee3
hritiklohiya <84116615+hritiklohiya@users.noreply.github.com>
2022-08-02 22:32:20 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('project4.pkl','rb')) 
+
+
+@app.route('/')
+def home():
+  
+    return render_template("index.html")
+  
+@app.route('/predict',methods=['GET'])
+
+def predict():
+    
+    
+    '''
+    For rendering results on HTML GUI
+    '''
+    PClass = float(request.args.get('PClass'))
+    Sex = float(request.args.get('Sex'))
+    Age = float(request.args.get('Age'))
+    Sibsp = float(request.args.get('Sibsp'))
+    Parch = float(request.args.get('Parch'))
+    Fare = float(request.args.get('Fare'))
+    
+    prediction = model.predict([[PClass,Sex,Age,Sibsp,Parch,Fare]])
+    
+    return render_template('index.html', prediction_text='KNN model to predict the people survived in titanic incident: {}'.format(prediction))
+
+    if prediction == 1:
+      print("Person Survived")
+    else:
+      print("Person doesn't Survived")
+
+app.run()
+
